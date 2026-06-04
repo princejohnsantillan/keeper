de5b7d195a0a62f766fb842c82d1ffc304268bf2
Prince John Santillan <mail@princejohnsantillan.net>
2026-06-04 10:25:19 +0800

    add children export

--- app/Filament/Panels/Keeper/Resources/Children/Pages/ListChildren.php
+++ app/Filament/Panels/Keeper/Resources/Children/Pages/ListChildren.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Filament\Panels\Keeper\Resources\Children\Pages;
 
+use App\Facades\Subdomain;
+use App\Filament\Exports\ChildExporter;
 use App\Filament\Panels\Keeper\Resources\Children\ChildResource;
+use Filament\Actions\ExportAction;
 use Filament\Resources\Pages\ListRecords;
 
 final class ListChildren extends ListRecords
@@ -13,6 +16,13 @@ final class ListChildren extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [];
+        return [
+            ExportAction::make()
+                ->label('Export children')
+                ->exporter(ChildExporter::class)
+                ->options([
+                    'organization_id' => Subdomain::organization()?->id,
+                ]),
+        ];
     }
 }
